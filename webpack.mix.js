b36321196398ed19dc9938dba481db38f5a3fd83
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-11-22 04:41:39 +0600

    assets and public folder renamed

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,48 +12,48 @@ const mix = require('laravel-mix');
  */
 
 mix.sourceMaps(true, 'source-map')
-    .setPublicPath('./public')
-    .setResourceRoot('./assets')
-    .copyDirectory('assets/images', 'public/images')
-    .copyDirectory('assets/vendor/ckeditor', 'public/js/ckeditor')
-    .copyDirectory('assets/vendor/css/minicolor', 'public/css/minicolor')
-    .copyDirectory('assets/fonts', 'public/fonts')
-    .copy('assets/vendor/js/modernizr.min.js', 'public/js/modernizr.min.js')
-    .copy('assets/css/custom.css', 'public/css/custom.css')
-    .copy('assets/js/custom.js', 'public/js/custom.js')
-    .copy('assets/vendor/js/jquery.minicolors.min.js', 'public/js/minicolors.js')
-    .minify('assets/js/scripts.js', 'public/js/scripts.min.js')
+    .setPublicPath('./dist')
+    .setResourceRoot('./src')
+    .copyDirectory('src/images', 'dist/images')
+    .copyDirectory('src/vendor/ckeditor', 'dist/js/ckeditor')
+    .copyDirectory('src/vendor/css/minicolor', 'dist/css/minicolor')
+    .copyDirectory('src/fonts', 'dist/fonts')
+    .copy('src/vendor/js/modernizr.min.js', 'dist/js/modernizr.min.js')
+    .copy('src/css/custom.css', 'dist/css/custom.css')
+    .copy('src/js/custom.js', 'dist/js/custom.js')
+    .copy('src/vendor/js/jquery.minicolors.min.js', 'dist/js/minicolors.js')
+    .minify('src/js/scripts.js', 'dist/js/scripts.min.js')
     .combine([
-        'assets/vendor/js/jquery.min.js',
-        'assets/vendor/js/popper.min.js',
-        'assets/vendor/js/photoswipe.min.js',
-        'assets/vendor/js/bootstrap.min.js',
-        'assets/vendor/js/downCount.min.js',
-        'assets/vendor/js/gmap3.min.js',
-        'assets/vendor/js/imagesloaded.pkgd.min.js',
-        'assets/vendor/js/isotope.pkgd.min.js',
-        'assets/vendor/js/iziToast.min.js',
-        'assets/vendor/js/nouislider.min.js',
-        'assets/vendor/js/numscroller.min.js',
-        'assets/vendor/js/owl.carousel.min.js',
-        'assets/vendor/js/photoswipe-ui-default.min.js',
-        'assets/vendor/js/velocity.min.js',
-        'assets/vendor/js/jquery.validate.min.js',
-        'assets/vendor/js/select2.min.js',
-        'assets/vendor/js/moment.min.js',
-        'assets/vendor/js/daterangepicker.min.js',
-        'assets/vendor/datatable/datatables.min.js',
-    ], 'public/js/vendor.min.js')
-    .sass('assets/scss/styles.scss', 'public/css/styles.css')
-    .minify('public/css/styles.css', 'public/css/styles.min.css')
+        'src/vendor/js/jquery.min.js',
+        'src/vendor/js/popper.min.js',
+        'src/vendor/js/photoswipe.min.js',
+        'src/vendor/js/bootstrap.min.js',
+        'src/vendor/js/downCount.min.js',
+        'src/vendor/js/gmap3.min.js',
+        'src/vendor/js/imagesloaded.pkgd.min.js',
+        'src/vendor/js/isotope.pkgd.min.js',
+        'src/vendor/js/iziToast.min.js',
+        'src/vendor/js/nouislider.min.js',
+        'src/vendor/js/numscroller.min.js',
+        'src/vendor/js/owl.carousel.min.js',
+        'src/vendor/js/photoswipe-ui-default.min.js',
+        'src/vendor/js/velocity.min.js',
+        'src/vendor/js/jquery.validate.min.js',
+        'src/vendor/js/select2.min.js',
+        'src/vendor/js/moment.min.js',
+        'src/vendor/js/daterangepicker.min.js',
+        'src/vendor/datatable/datatables.min.js',
+    ], 'dist/js/vendor.min.js')
+    .sass('src/scss/styles.scss', 'dist/css/styles.css')
+    .minify('dist/css/styles.css', 'dist/css/styles.min.css')
     .combine([
-        'assets/vendor/css/select2.min.css',
-        'assets/vendor/css/bootstrap.min.css',
-        'assets/vendor/css/feather.min.css',
-        'assets/vendor/css/iziToast.min.css',
-        'assets/vendor/css/pe-icon-7-stroke.min.css',
-        'assets/vendor/css/photoswipe.min.css',
-        'assets/vendor/css/socicon.min.css',
-        'assets/vendor/css/daterangepicker.css',
-        'assets/vendor/datatable/datatables.min.css',
-    ], 'public/css/vendor.min.css');
+        'src/vendor/css/select2.min.css',
+        'src/vendor/css/bootstrap.min.css',
+        'src/vendor/css/feather.min.css',
+        'src/vendor/css/iziToast.min.css',
+        'src/vendor/css/pe-icon-7-stroke.min.css',
+        'src/vendor/css/photoswipe.min.css',
+        'src/vendor/css/socicon.min.css',
+        'src/vendor/css/daterangepicker.css',
+        'src/vendor/datatable/datatables.min.css',
+    ], 'dist/css/vendor.min.css');
